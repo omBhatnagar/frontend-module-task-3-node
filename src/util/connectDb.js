c09c0e97be8a2f9e6877fe494b9e4699c193b62d
Om Bhatnagar <omishb2001@gmail.com>
2023-04-24 22:48:06 +0530

    Added db url as environment variable in docker compose.

--- src/util/connectDb.js
+++ src/util/connectDb.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = () => {
 	const db_uri = process.env.DATABASE_URL;
-	mongoose.connect(
-		"mongodb+srv://omishb2001:Omi%40ch82%2311%40mongoDB@meanmerntask.22g6zba.mongodb.net/test",
-	);
+	mongoose.connect(db_uri);
 	const database = mongoose.connection;
 
 	database.on("error", (error) => console.log(error));
